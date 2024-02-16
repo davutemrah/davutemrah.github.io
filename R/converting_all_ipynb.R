a65091e502b7d88b61e1ccf4468cb9add1b50e58
Davut E. Ayan <demrahayan@gmail.com>
2024-02-15 20:19:26 -0600

    update feb 15

---
+++ R/converting_all_ipynb.R
@@ -0,0 +1,16 @@
+# Get list of .ipynb files
+file_list <- list.files("content/post", pattern = "\\.ipynb$",
+                        full.names = TRUE, recursive = TRUE)
+
+# Loop over each .ipynb file
+for (file_path in file_list) {
+  # Get the new file name with .Rmd extension
+  new_file_name <- gsub("\\.ipynb$", ".Rmd", file_path)
+  # Convert .ipynb to .Rmd
+  # jupytext::jupytext(file_path, to="Rmd", output= new_file_name)
+  rmarkdown::convert_ipynb(file_path, new_file_name)
+  print(file_path)
+  print(new_file_name)
+
+}
+
